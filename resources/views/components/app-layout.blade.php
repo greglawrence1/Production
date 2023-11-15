d3acc4da7b317223fb1c34472c13fecfb7df054e
c3625174 <g.lawrence6468@student.leedsbeckett.ac.uk>
2023-11-15 10:28:11 +0000

    blade app layout

---
+++ resources/views/components/app-layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ASE</title>
+</head>
+<body>
+    <h1>NAV BAR</h1>
+
+    {{ $slot}}
+</body>
+</html>
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,6 +1,7 @@
-
+<x-app-layout>
     <div>
     @foreach($products as $product)
         <x-product-card :product="$product"/>
     @endforeach
     </div>
+</x-app-layout>
