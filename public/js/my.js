aa585db183db1930f0eb77d4884b475af4b6634f
c3625174 <g.lawrence6468@student.leedsbeckett.ac.uk>
2023-12-09 17:31:06 +0000

    tested the javascript file, in the blade file, it works, added the js file to the public folder, still not working just yet

---
+++ public/js/my.js
@@ -0,0 +1,51 @@
+window.onload = function () {
+
+    document.addEventListener('change', e => {
+        if (e.target.matches('select.select-box')) {
+            filterByProductType(e.target.value);
+        }
+    });
+    document.addEventListener('click', e => {
+        if (e.target.matches('button.select-product')) {
+            console.log('button pressed');
+            getProductByID(e.target.value);
+        }
+        if (e.target.matches('button.update-product')) {
+            updateProductByID(e.target.value);
+        }
+        if (e.target.matches('button.buy-product')) {
+            buyProductByID(e.target.value);
+        }
+    });
+}
+
+function buyProductByID(id) {
+    alert('product purchased');
+}
+
+function getProductByID(id) {
+    window.location = "/product/" + id;
+}
+
+function updateProductByID(id) {
+    window.location = "/product/" + id + "/edit";
+}
+
+async function filterByProductType(id) {
+    if (id == 0) window.location = "/products/"
+    else {
+        try {
+            const response = await axios.get('/filter',
+                {params: {producttype: id}}
+            );
+
+            var filter = document.getElementById("productlist");
+            filter.innerHTML = response.data;
+            var pagination = document.getElementById("pagination");
+            pagination.innerHTML = "<br><br>";
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-        <script src="{{asset ('js/my.js')}}"></script>
+        <script src="{{asset('js/my.js') }}"></script>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
