c2fede7e2ceb0fcbd6bc367bfd986773bbb4a472
c3625174 <g.lawrence6468@student.leedsbeckett.ac.uk>
2024-01-03 01:34:13 +0000

    made my single product view use 3 components

--- resources/views/singleproduct.blade.php
+++ resources/views/singleproduct.blade.php
@@ -1,7 +1,9 @@
 <x-app-layout>
+<x-application-logo/>
 <a href="{{ url('')}}">Home</a>
 <a href="{{ url('/product/create')}}">Add Products</a>
     <div class="grid grid-cols-4">
             <x-product-card :product="$product"/>
     </div>
+   
 </x-app-layout>
