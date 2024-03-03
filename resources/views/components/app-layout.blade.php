77a91a33c5568feba32cbdfde2e3415ed024f2b6
Gregory Lawrence <greg_lawrence2017@outlook.com>
2024-03-03 11:25:36 +0000

    up to dating it

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -5,14 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ASE</title>
     @vite('resources/css/app.css')
-  
+    
 
     <script src="{{asset('js/app.js') }}"></script>
     <script src="{{asset('js/bootstrap.js') }}"></script>
     <script src="{{asset('js/my.js') }}"></script>
 </head>
 
-<body class="background">
+<body style="background-image: url('images/background.jpg'); background-size: cover; background-position: center; z-index: -1;">
     
     @include('menu')
 
--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -5,13 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ASE</title>
     @vite('resources/css/app.css')
-  
-
+ 
     <script src="{{asset('js/app.js') }}"></script>
     <script src="{{asset('js/bootstrap.js') }}"></script>
     <script src="{{asset('js/my.js') }}"></script>
 </head>
-<body class="">
+<body style="background-image: url('images/background.jpg'); background-size: cover; background-position: center; z-index: -1;">
    <nav class="bg-gray-800 shadow z-10 h-16">
     <div class="container mx-auto px-4">
         <div class="flex items-center justify-between h-16">
